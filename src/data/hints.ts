ada4a2b69033f93c5f5e0debb08c97ffdbf3d5ec
Mark <mark@remarkablemark.org>
2025-11-29 18:32:48 -0500

    refactor(data): modularize hints

---
+++ src/data/hints.ts
@@ -0,0 +1,19 @@
+export const hints = [
+  // 0
+  {
+    text: 'Drag and drop the hero (bottom) to the island (center) and press "Start"',
+    height: 80,
+  },
+
+  // 1
+  {
+    text: 'Every hero has a unique attack',
+    height: 50,
+  },
+
+  // 2
+  {
+    text: 'Look for good strategies to defeat the enemies',
+    height: 60,
+  },
+]
--- src/gameobjects/hint.ts
+++ src/gameobjects/hint.ts
@@ -1,25 +1,5 @@
 import { Tag } from '../constants'
-import { state } from '../data'
-
-const hints = [
-  // 0
-  {
-    text: 'Drag and drop the hero (bottom) to the island (center) and press "Start"',
-    height: 80,
-  },
-
-  // 1
-  {
-    text: 'Every hero has a unique attack',
-    height: 50,
-  },
-
-  // 2
-  {
-    text: 'Look for good strategies to defeat the enemies',
-    height: 60,
-  },
-]
+import { hints, state } from '../data'
 
 const width = 400
 
